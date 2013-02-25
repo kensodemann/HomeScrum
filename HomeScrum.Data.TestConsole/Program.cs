196c747fe5860c080380edae196f5b8a678e900d
Ken Sodemann <ken.sodemann@gmail.com>
2013-02-24 20:41:38 -0600

    Add  console tests for new items.

--- HomeScrum.Data.TestConsole/Program.cs
+++ HomeScrum.Data.TestConsole/Program.cs
@@ -10,6 +10,66 @@ namespace HomeScrum.Data.TestConsole
 {
    public class Program
    {
+      static void Main( string[] args )
+      {
+         PrintAllWorkItemTypes();
+         PrintAllWorkItemStatuses();
+         PrintAllSprintStatuses();
+         PrintAllProjectStatuses();
+      }
+
+      private static void PrintAllWorkItemTypes()
+      {
+         var repository = new DataObjectRepository<WorkItemType>();
+         var workItemTypes = repository.GetAll();
+
+         Console.WriteLine( "Work Item Types: " + workItemTypes.Count().ToString() );
+
+         foreach (var workItemType in workItemTypes)
+         {
+            PrintWorkItemType( workItemType );
+         }
+      }
+
+      private static void PrintAllWorkItemStatuses()
+      {
+         var repository = new DataObjectRepository<WorkItemStatus>();
+         var workItemStatuses = repository.GetAll();
+
+         Console.WriteLine( "Work Item Statuses: " + workItemStatuses.Count().ToString() );
+
+         foreach (var workItemStatus in workItemStatuses)
+         {
+            PrintWorkItemStatus( workItemStatus );
+         }
+      }
+
+      private static void PrintAllSprintStatuses()
+      {
+         var repository = new DataObjectRepository<SprintStatus>();
+         var sprintStatuses = repository.GetAll();
+
+         Console.WriteLine( "Sprint Statuses: " + sprintStatuses.Count().ToString() );
+
+         foreach (var sprintStatus in sprintStatuses)
+         {
+            PrintSprintStatus( sprintStatus );
+         }
+      }
+
+      private static void PrintAllProjectStatuses()
+      {
+         var repository = new DataObjectRepository<ProjectStatus>();
+         var projectStatuses = repository.GetAll();
+
+         Console.WriteLine( "Project Statuses: " + projectStatuses.Count().ToString() );
+
+         foreach (var projectStatus in projectStatuses)
+         {
+            PrintProjectStatus( projectStatus );
+         }
+      }
+
       static void PrintWorkItemType( WorkItemType workItemType )
       {
          Console.WriteLine( "\nWorkItemType\n" );
@@ -21,18 +81,37 @@ namespace HomeScrum.Data.TestConsole
          Console.WriteLine( "\tIsPredefined: " + workItemType.IsPredefined );
       }
 
-
-      static void Main( string[] args )
+      static void PrintWorkItemStatus( WorkItemStatus workItemStatus )
       {
-         var repository = new DataObjectRepository<WorkItemType>();
-         var workItemTypes = repository.GetAll();
+         Console.WriteLine( "\nWorkItemStatus\n" );
+         Console.WriteLine( "\tId: " + workItemStatus.Id.ToString() );
+         Console.WriteLine( "\tName: " + workItemStatus.Name );
+         Console.WriteLine( "\tDescription: " + workItemStatus.Description );
+         Console.WriteLine( "\tStatusCd: " + workItemStatus.StatusCd );
+         Console.WriteLine( "\tIsOpenStatus: " + workItemStatus.IsOpenStatus );
+         Console.WriteLine( "\tIsPredefined: " + workItemStatus.IsPredefined );
+      }
 
-         Console.WriteLine( "Work Item Types: " + workItemTypes.Count().ToString() );
+      static void PrintSprintStatus( SprintStatus sprintStatus )
+      {
+         Console.WriteLine( "\nSprintStatus\n" );
+         Console.WriteLine( "\tId: " + sprintStatus.Id.ToString() );
+         Console.WriteLine( "\tName: " + sprintStatus.Name );
+         Console.WriteLine( "\tDescription: " + sprintStatus.Description );
+         Console.WriteLine( "\tStatusCd: " + sprintStatus.StatusCd );
+         Console.WriteLine( "\tIsOpenStatus: " + sprintStatus.IsOpenStatus );
+         Console.WriteLine( "\tIsPredefined: " + sprintStatus.IsPredefined );
+      }
 
-         foreach (var workItemType in workItemTypes)
-         {
-            PrintWorkItemType( workItemType );
-         }
+      static void PrintProjectStatus( ProjectStatus projectStatus )
+      {
+         Console.WriteLine( "\nProjectStatus\n" );
+         Console.WriteLine( "\tId: " + projectStatus.Id.ToString() );
+         Console.WriteLine( "\tName: " + projectStatus.Name );
+         Console.WriteLine( "\tDescription: " + projectStatus.Description );
+         Console.WriteLine( "\tStatusCd: " + projectStatus.StatusCd );
+         Console.WriteLine( "\tIsActive: " + projectStatus.IsActive );
+         Console.WriteLine( "\tIsPredefined: " + projectStatus.IsPredefined );
       }
    }
 }
