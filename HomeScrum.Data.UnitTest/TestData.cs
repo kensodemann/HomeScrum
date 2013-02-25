03862d13a6edb194967e12dc77b9bb7d311e224b
Ken Sodemann <ken.sodemann@gmail.com>
2013-02-24 19:28:15 -0600

    Add work item status model.

--- HomeScrum.Data.UnitTest/TestData.cs
+++ HomeScrum.Data.UnitTest/TestData.cs
@@ -27,8 +27,108 @@ namespace HomeScrum.Data.UnitTest
       public static void BuildDatabase()
       {
          new SchemaExport( _configuration ).Execute( false, true, false );
+         CreateInitialWorkItemStatuses();
          CreateInitialWorkItemTypes();
       }
+
+      #region WorkItemStatuses
+      private static void CreateInitialWorkItemStatuses()
+      {
+
+         using (ISession session = _sessionFactory.OpenSession())
+         using (ITransaction transaction = session.BeginTransaction())
+         {
+            foreach (var workItemStatus in WorkItemStatuses)
+               session.Save( workItemStatus );
+            transaction.Commit();
+         }
+      }
+
+      public static readonly WorkItemStatus[] WorkItemStatuses = new[]
+      {
+         new WorkItemStatus ()
+         {
+            Id=Guid.NewGuid(),
+            Name="New",
+            Description="The Item is brand new",
+            StatusCd='A',
+            IsActive='Y',
+            IsPredefined='Y'
+         },
+         new WorkItemStatus ()
+         {
+            Id=Guid.NewGuid(),
+            Name="In Process",
+            Description="The Item is being worked on",
+            StatusCd='A',
+            IsActive='Y',
+            IsPredefined='Y'
+         },
+         new WorkItemStatus ()
+         {
+            Id=Guid.NewGuid(),
+            Name="On Hold",
+            Description="The Item was started but cannot be worked on",
+            StatusCd='A',
+            IsActive='Y',
+            IsPredefined='Y'
+         },
+         new WorkItemStatus ()
+         {
+            Id=Guid.NewGuid(),
+            Name="Ready for Test",
+            Description="The Item is ready to be tested",
+            StatusCd='A',
+            IsActive='Y',
+            IsPredefined='Y'
+         },
+         new WorkItemStatus ()
+         {
+            Id=Guid.NewGuid(),
+            Name="Complete",
+            Description="The Item is done",
+            StatusCd='A',
+            IsActive='Y',
+            IsPredefined='Y'
+         },
+         new WorkItemStatus ()
+         {
+            Id=Guid.NewGuid(),
+            Name="Status 1",
+            Description="Active Status, Active, Predefined",
+            StatusCd='A',
+            IsActive='Y',
+            IsPredefined='Y'
+         },
+         new WorkItemStatus ()
+         {
+            Id=Guid.NewGuid(),
+            Name="Status 2",
+            Description="Inactive Status, Active, Predefined",
+            StatusCd='I',
+            IsActive='Y',
+            IsPredefined='Y'
+         },
+         new WorkItemStatus ()
+         {
+            Id=Guid.NewGuid(),
+            Name="Status 1",
+            Description="Active Status, Not Active, Predefined",
+            StatusCd='A',
+            IsActive='N',
+            IsPredefined='Y'
+         },
+         new WorkItemStatus ()
+         {
+            Id=Guid.NewGuid(),
+            Name="Status 1",
+            Description="Active Status, Active, Not Predefined",
+            StatusCd='A',
+            IsActive='Y',
+            IsPredefined='N'
+         }
+      };
+      #endregion
 
       #region WorkItemTypes
       private static void CreateInitialWorkItemTypes()
---
+++ HomeScrum.Data/Domain/WorkItemStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HomeScrum.Data.Domain
+{
+   public class WorkItemStatus : BaseDataObject
+   {
+      public virtual char StatusCd { get; set; }
+      public virtual char IsActive { get; set; }
+      public virtual char IsPredefined { get; set; }
+   }
+}
