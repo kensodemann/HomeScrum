15ed146ec524f947035b45c433a02e037e18f04f
Ken Sodemann <ken.sodemann@gmail.com>
2013-02-26 18:30:12 -0600

    Add admin controller and view

---
+++ HomeScrum.Web/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeScrum.Data.Repositories;
+using HomeScrum.Data.Domain;
+using HomeScrum.Data.SqlServer;
+
+namespace HomeScrum.Web.Controllers
+{
+   public class AdminController : Controller
+   {
+      public ActionResult Index()
+      {
+         // TODO: Inject this...
+         IDataObjectRepository<WorkItemType> repository = new DataObjectRepository<WorkItemType>();
+
+         return View( repository.GetAll() );
+      }
+
+   }
+}
