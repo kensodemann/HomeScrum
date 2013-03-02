d11411b1c4e529ca697d75eb4729148d8e03ba50
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-02 08:01:16 -0600

    Do some injections.

--- HomeScrum.Web/Controllers/AdminController.cs
+++ HomeScrum.Web/Controllers/AdminController.cs
@@ -1,16 +1,40 @@
-﻿using System;
+﻿using HomeScrum.Data.Domain;
+using HomeScrum.Data.Repositories;
+using HomeScrum.Data.SqlServer;
+using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using HomeScrum.Data.Repositories;
-using HomeScrum.Data.Domain;
-using HomeScrum.Data.SqlServer;
 
 namespace HomeScrum.Web.Controllers
 {
    public class AdminController : Controller
    {
+      #region Contstruction
+      private readonly IDataObjectRepository<WorkItemStatus> _statusRepository;
+      private readonly IDataObjectRepository<WorkItemType> _typeRepository;
+
+      [Inject]
+      public AdminController( IDataObjectRepository<WorkItemStatus> statusRepository,
+                              IDataObjectRepository<WorkItemType> typeRepository )
+      {
+         _statusRepository = statusRepository;
+         _typeRepository = typeRepository;
+      }
+      #endregion
+
+      // TODOs:
+      //
+      // Immediate:
+      //  Create tests for this.
+      //  Create initial pages, etc for the other base items.
+      //
+      // Long Term:
+      //  Create view models and use them instead.  This may move the repository injection points,
+      //  especially if we inject the view models.
+
       // GET: /Admin/
       public ActionResult Index()
       {
@@ -21,21 +45,15 @@ namespace HomeScrum.Web.Controllers
       // GET: /Admin/WorkItemTypes
       public ActionResult WorkItemTypes()
       {
-         // TODO: Inject this...
-         IDataObjectRepository<WorkItemType> repository = new DataObjectRepository<WorkItemType>();
-
-         return View( repository.GetAll() );
+         return View( _typeRepository.GetAll() );
       }
 
 
       // GET: /Admin/WorkItemStatuses
       public ActionResult WorkItemStatuses()
       {
-         // TODO: Inject this...
          // Unltimately, we should have a view model for this instead of the data model
-         IDataObjectRepository<WorkItemStatus> repository = new DataObjectRepository<WorkItemStatus>();
-
-         return View( repository.GetAll() );
+         return View( _statusRepository.GetAll() );
       }
    }
 }
