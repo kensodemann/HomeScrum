d5685f6cb512225398eb882edcc6c06aef0c7e33
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-02 14:59:24 -0600

    Begin controller test creation.

---
+++ HomeScrum.Web.UnitTest/AdminControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using HomeScrum.Data.Domain;
+using HomeScrum.Data.Repositories;
+using HomeScrum.Web.Controllers;
+using HomeScrum.Common.TestData;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+
+namespace HomeScrum.Web.UnitTest
+{
+   [TestClass]
+   public class AdminControllerTest
+   {
+      [TestMethod]
+      public void WorkItemTypes_GetsAllWorkItemTypes()
+      {
+         var repository = new Mock<IDataObjectRepository<WorkItemType>>();
+
+         var controller = new AdminController( null, repository.Object );
+         controller.WorkItemTypes();
+
+         repository.Verify( x => x.GetAll(), Times.Once() );
+      }
+
+      [TestMethod]
+      public void WorkItemTypes_ReturnsViewWithModel()
+      {
+         var repository = new Mock<IDataObjectRepository<WorkItemType>>();
+
+         repository.Setup( x => x.GetAll() )
+            .Returns( WorkItemTypes.ModelData );
+
+         var controller = new AdminController( null, repository.Object );
+         var view = controller.WorkItemTypes() as ViewResult;
+
+         Assert.IsNotNull( view );
+         Assert.IsNotNull( view.Model );
+      }
+   }
+}
