b5c99a99e5c854c120ffea1d2b32e4d1f7b97824
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-07 19:59:15 -0600

    Test Default constructor

---
+++ HomeScrum.Web.UnitTest/ViewModels/DataObjectBaseViewModelTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HomeScrum.Data.Domain;
+using HomeScrum.Web.Models;
+
+namespace HomeScrum.Web.UnitTest.ViewModels
+{
+   [TestClass]
+   public class DataObjectBaseViewModelTest
+   {
+      [TestMethod]
+      public void DefaultConstructor_CreatesEmptyModel()
+      {
+         var viewModel = new DataObjectBaseViewModel<DataObjectBase>();
+
+         Assert.IsNotNull( viewModel.Model );
+         Assert.AreEqual( default( Guid ), viewModel.Model.Id );
+         Assert.IsTrue( String.IsNullOrEmpty( viewModel.Model.Name ) );
+         Assert.IsTrue( string.IsNullOrEmpty( viewModel.Model.Description ) );
+      }
+   }
+}
