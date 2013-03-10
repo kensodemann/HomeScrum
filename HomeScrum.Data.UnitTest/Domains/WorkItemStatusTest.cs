5c4eff70cd6178a2e4b80a31d5bc965fe55e9ec3
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-10 06:58:45 -0500

    Add work item status display data.

---
+++ HomeScrum.Data.UnitTest/Domains/WorkItemStatusTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HomeScrum.Data.Domain;
+using HomeScrum.Data.UnitTest.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomeScrum.Data.UnitTest.Domains
+{
+   [TestClass]
+   public class WorkItemStatusTest
+   {
+      [TestMethod]
+      public void IsOpenDisplayName()
+      {
+         var display = AttributeHelper.GetDisplayAttribute( typeof( WorkItemStatus ), "IsOpenStatus" );
+
+         Assert.IsNotNull( display );
+         Assert.AreEqual( typeof( DisplayStrings ), display.ResourceType );
+         Assert.AreEqual( "WorkItemStatusIsOpenStatus", display.Name );
+         Assert.IsFalse( String.IsNullOrWhiteSpace( DisplayStrings.WorkItemStatusIsOpenStatus ) );
+      }
+   }
+}
--- HomeScrum.Data/Domain/WorkItemStatus.cs
+++ HomeScrum.Data/Domain/WorkItemStatus.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomeScrum.Data.Domain
 {
    public class WorkItemStatus : SystemDataObject
    {
+      [Display( Name = "WorkItemStatusIsOpenStatus", ResourceType = typeof( DisplayStrings ) )]
       public virtual bool IsOpenStatus { get; set; }
    }
 }
