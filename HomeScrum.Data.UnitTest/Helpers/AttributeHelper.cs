2f03d5b4f8ae0380ae1b8795d36e342376086d10
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-09 19:15:35 -0600

    somehow got unchecked...

---
+++ HomeScrum.Data.UnitTest/Helpers/AttributeHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeScrum.Data.UnitTest.Helpers
+{
+   class AttributeHelper
+   {
+      public static DisplayAttribute GetDisplayAttribute( Type modelType, string propertyName )
+      {
+         return GetAttribute( modelType, typeof(DisplayAttribute), propertyName ) as DisplayAttribute;
+      }
+
+      private static Attribute GetAttribute( Type modelType, Type attributeType, string propertyName )
+      {
+         var propertyInfo = modelType.GetProperty( propertyName );
+
+         return (propertyInfo != null)
+            ? Attribute.GetCustomAttribute( propertyInfo, attributeType )
+            : null;
+      }
+   }
+}
