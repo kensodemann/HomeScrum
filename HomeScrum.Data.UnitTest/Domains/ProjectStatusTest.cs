a14ec5ef870538bfacdcddf2ac04ca662aea3a00
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-10 06:43:55 -0500

    Add project status is active display

---
+++ HomeScrum.Data.UnitTest/Domains/ProjectStatusTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HomeScrum.Data.Domain;
+using HomeScrum.Data.UnitTest.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomeScrum.Data.UnitTest.Domains
+{
+   [TestClass]
+   public class ProjectStatusTest
+   {
+      [TestMethod]
+      public void IsActiveDisplayName()
+      {
+         var display = AttributeHelper.GetDisplayAttribute( typeof( ProjectStatus ), "IsActive" );
+
+         Assert.IsNotNull( display );
+         Assert.AreEqual( typeof( DisplayStrings ), display.ResourceType );
+         Assert.AreEqual( "ProjectStatusIsActive", display.Name );
+         Assert.IsFalse( String.IsNullOrWhiteSpace( DisplayStrings.ProjectStatusIsActive ) );
+      }
+   }
+}
