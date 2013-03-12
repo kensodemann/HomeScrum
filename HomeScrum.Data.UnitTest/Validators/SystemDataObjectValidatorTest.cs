f86d7dd0734decbc3e14946b1808ff9e4986ce14
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-11 20:51:44 -0500

    Begin refitting of the validator code.

---
+++ HomeScrum.Data.UnitTest/Validators/SystemDataObjectValidatorTest.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HomeScrum.Data.Validators;
+using Moq;
+using HomeScrum.Data.Domain;
+using HomeScrum.Data.Repositories;
+using HomeScrum.Common.TestData;
+
+namespace HomeScrum.Data.UnitTest.Validators
+{
+   [TestClass]
+   public class SystemDataObjectValidatorTest
+   {
+      private Mock<IDataObjectRepository<WorkItemType>> _repository;
+      private SystemDataObjectValidator<WorkItemType> _validator;
+
+      [TestInitialize]
+      public void InitializeTest()
+      {
+         _repository = new Mock<IDataObjectRepository<WorkItemType>>();
+         _repository.Setup( x => x.GetAll() ).Returns( WorkItemTypes.ModelData );
+         _validator = new SystemDataObjectValidator<WorkItemType>( _repository.Object );
+      }
+
+
+      //[TestMethod]
+      //public void ValidatorGetsAllWorkItemTypes_IfValueWorkItemType()
+      //{
+      //   var model = new WorkItemType();
+      //   var result = _validator.GetValidationResult( model, new ValidationContext( model, null, null ) );
+
+      //   _repository.Verify( x => x.GetAll(), Times.Once() );
+      //}
+
+      //[TestMethod]
+      //public void ValidatorDoesNotGetAll_IfValueNotWorkItemType()
+      //{
+      //   var model = new WorkItemStatus();
+      //   model.Name = WorkItemTypes.ModelData[1].Name;
+      //   _validator.ErrorMessage = "This is my error message";
+
+      //   var result = _validator.GetValidationResult( model, new ValidationContext( model, null, null ) );
+
+      //   _repository.Verify( x => x.GetAll(), Times.Never() );
+      //}
+
+      //[TestMethod]
+      //public void ValidatorReturnsSuccess_IfValueNotWorkItemType()
+      //{
+      //   var model = new WorkItemStatus();
+      //   model.Name = WorkItemTypes.ModelData[1].Name;
+      //   _validator.ErrorMessage = "This is my error message";
+
+      //   var result = _validator.GetValidationResult( model, new ValidationContext( model, null, null ) );
+
+      //   Assert.AreEqual( ValidationResult.Success, result );
+      //}
+
+      //[TestMethod]
+      //public void ValidatorReturnsSuccess_IfValueNameEmpty()
+      //{
+      //   var model = new WorkItemStatus();
+      //   _validator.ErrorMessage = "This is my error message";
+
+      //   var result = _validator.GetValidationResult( model, new ValidationContext( model, null, null ) );
+
+      //   Assert.AreEqual( ValidationResult.Success, result );
+      //}
+
+      //[TestMethod]
+      //public void ValidatorReturnsErrorMessage_IfNameExistsOnNewObject()
+      //{
+      //   var model = new WorkItemType();
+      //   model.Name = WorkItemTypes.ModelData[1].Name;
+      //   _validator.ErrorMessage = "This is my error message";
+
+      //   var result = _validator.GetValidationResult( model, new ValidationContext( model, null, null ) );
+
+      //   Assert.AreNotEqual( ValidationResult.Success, result );
+      //   Assert.AreEqual( "This is my error message", result.ErrorMessage );
+      //}
+
+      //[TestMethod]
+      //public void ValidatorReturnsErrorMessage_IfNameExistsOnValueWithDifferentId()
+      //{
+      //   var model = new WorkItemType();
+      //   model.Name = WorkItemTypes.ModelData[1].Name;
+      //   model.Id = WorkItemTypes.ModelData[0].Id;
+      //   _validator.ErrorMessage = "This is my error message";
+
+      //   var result = _validator.GetValidationResult( model, new ValidationContext( model, null, null ) );
+
+      //   Assert.AreNotEqual( ValidationResult.Success, result );
+      //   Assert.AreEqual( "This is my error message", result.ErrorMessage );
+      //}
+
+      //[TestMethod]
+      //public void ValidatorReturnsSuccess_IfNameExistsOnValueWithMatchingId()
+      //{
+      //   var model = new WorkItemType();
+      //   model.Name = WorkItemTypes.ModelData[1].Name;
+      //   model.Id = WorkItemTypes.ModelData[1].Id;
+      //   _validator.ErrorMessage = "This is my error message";
+
+      //   var result = _validator.GetValidationResult( model, new ValidationContext( model, null, null ) );
+
+      //   Assert.AreEqual( ValidationResult.Success, result );
+      //}
+
+      //[TestMethod]
+      //public void ValidatorUsesResourceString_IfValidResourceGiven()
+      //{
+      //   var model = new WorkItemType();
+      //   model.Name = WorkItemTypes.ModelData[1].Name;
+      //   _validator.ErrorMessage = "This is my error message";
+      //   _validator.ErrorMessageResourceName = "TestErrorMessage";
+      //   _validator.ErrorMessageResourceType = typeof( TestStrings );
+
+      //   var result = _validator.GetValidationResult( model, new ValidationContext( model, null, null ) );
+
+      //   Assert.AreEqual( TestStrings.TestErrorMessage, result.ErrorMessage );
+      //}
+
+      // TODO: Add test for:
+      //    o rename the validator class
+      //    o incomplete resource (give no ErrorMessageResourceName)
+      //    o invalid resource (give bad ErrorMessageResourceName)
+      //    o no error message given (use default generic message)
+   }
+}
---
+++ HomeScrum.Data/Validators/SystemDataObjectValidator.cs
@@ -0,0 +1,65 @@
+﻿using HomeScrum.Data.Domain;
+using HomeScrum.Data.Repositories;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeScrum.Data.Validators
+{
+   public class SystemDataObjectValidator<DataObjectType> : IValidator<DataObjectType> where DataObjectType : SystemDataObject
+   {
+      public SystemDataObjectValidator( IDataObjectRepository<DataObjectType> repository )
+      {
+         Repository = repository;
+      }
+
+      public IDataObjectRepository<DataObjectType> Repository { get; private set; }
+
+      public bool ModelIsValid( DataObjectType model )
+      {
+         throw new NotImplementedException();
+      }
+
+      public ICollection<KeyValuePair<string, string>> Messages
+      {
+         get { throw new NotImplementedException(); }
+      }
+      //protected override ValidationResult IsValid( object value, ValidationContext validationContext )
+      //{
+      //   if (WorkItemTypeWithSameNameExists( value ))
+      //   {
+      //      return new ValidationResult( GetErrorMessage() );
+      //   }
+
+      //   return ValidationResult.Success;
+      //}
+
+      //private bool WorkItemTypeWithSameNameExists( object value )
+      //{
+      //   WorkItemType typeWithSameName = null;
+
+      //   var workItemType = value as WorkItemType;
+      //   if (workItemType != null)
+      //   {
+      //      typeWithSameName = this.Repository.GetAll()
+      //         .FirstOrDefault( x => x.Name == workItemType.Name && x.Id != workItemType.Id );
+      //   }
+
+      //   return typeWithSameName != null;
+      //}
+
+      //private string GetErrorMessage()
+      //{
+      //   if (!String.IsNullOrWhiteSpace( this.ErrorMessageResourceName ) && this.ErrorMessageResourceType != null)
+      //   {
+      //      return ErrorMessageResourceType.InvokeMember( this.ErrorMessageResourceName,
+      //         System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.GetProperty,
+      //         null, null, null ).ToString();
+      //   }
+      //   return this.ErrorMessage;
+      //}
+   }
+}
