5911c7786aca73b652f4683d12c57072417ee098
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-11 20:39:48 -0500

    Add IValidator interface.

--- HomeScrum.Data/Domain/WorkItemType.cs
+++ HomeScrum.Data/Domain/WorkItemType.cs
@@ -1,10 +1,8 @@
-﻿using HomeScrum.Data.Validators;
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace HomeScrum.Data.Domain
 {
-   [ValidateWorkItemType]
    public class WorkItemType : SystemDataObject
    {
       [Display( Name = "WorkItemTypeIsTask", ResourceType = typeof( DisplayStrings ) )]
---
+++ HomeScrum.Data/Validators/IValidator.cs
@@ -0,0 +1,15 @@
+﻿using HomeScrum.Data.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeScrum.Data.Validators
+{
+   public interface IValidator<DataObjectType> where DataObjectType : DataObjectBase
+   {
+      bool ModelIsValid( DataObjectType model );
+      ICollection<KeyValuePair<string, string>> Messages { get; }
+   }
+}
