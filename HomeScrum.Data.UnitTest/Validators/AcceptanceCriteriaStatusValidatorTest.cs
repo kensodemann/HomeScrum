97756b7bcfc61de06ab36c17dc66b3476db310b4
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-13 09:49:04 -0500

    Create validator for acceptance criteria statuses

---
+++ HomeScrum.Data.UnitTest/Validators/AcceptanceCriteriaStatusValidatorTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HomeScrum.Data.Domain;
+using HomeScrum.Common.TestData;
+using Moq;
+using HomeScrum.Data.Validators;
+using HomeScrum.Data.Repositories;
+
+namespace HomeScrum.Data.UnitTest.Validators
+{
+   [TestClass]
+   public class AcceptanceCriteriaStatusValidatorTest
+   {
+
+      private Mock<IDataObjectRepository<AcceptanceCriteriaStatus>> _repository;
+      private AcceptanceCriteriaStatusValidator _validator;
+
+      [TestInitialize]
+      public void InitializeTest()
+      {
+         _repository = new Mock<IDataObjectRepository<AcceptanceCriteriaStatus>>();
+         _repository.Setup( x => x.GetAll() ).Returns( AcceptanceCriteriaStatuses.ModelData );
+         _validator = new AcceptanceCriteriaStatusValidator( _repository.Object );
+      }
+
+
+      [TestMethod]
+      public void MessagesContainsUniqueNameMessage_IfNameNotUnique()
+      {
+         var model = new AcceptanceCriteriaStatus();
+         model.Name = AcceptanceCriteriaStatuses.ModelData[1].Name;
+         model.Id = AcceptanceCriteriaStatuses.ModelData[0].Id;
+
+         var result = _validator.ModelIsValid( model );
+
+         Assert.AreEqual( 1, _validator.Messages.Count );
+         foreach (var message in _validator.Messages)
+         {
+            Assert.AreEqual( "Name", message.Key );
+            Assert.AreEqual( String.Format( ErrorMessages.NameIsNotUnique, "Acceptance Criteria Status", model.Name ), message.Value );
+         }
+      }
+   }
+}
--- HomeScrum.Data/Validators/AcceptanceCriteriaStatusValidator.cs
+++ HomeScrum.Data/Validators/AcceptanceCriteriaStatusValidator.cs
@@ -8,9 +8,12 @@ using System.Threading.Tasks;
 
 namespace HomeScrum.Data.Validators
 {
-   class AcceptanceCriteriaStatusValidator : SystemDataObjectValidator<AcceptanceCriteriaStatus>
+   public class AcceptanceCriteriaStatusValidator : SystemDataObjectValidator<AcceptanceCriteriaStatus>
    {
       public AcceptanceCriteriaStatusValidator( IDataObjectRepository<AcceptanceCriteriaStatus> repository )
          : base( repository ) { }
+
+
+      protected override string ObjectName { get { return "Acceptance Criteria Status"; } }
    }
 }
