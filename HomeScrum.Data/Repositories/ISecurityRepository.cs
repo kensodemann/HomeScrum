414655b3332ac842dd06e57b9d60fcc83f9b6c0b
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-16 16:01:39 -0500

    Add a security mapping.

---
+++ HomeScrum.Data/Repositories/ISecurityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeScrum.Data.Repositories
+{
+   public interface ISecurityRepository
+   {
+      bool IsValidLogin( string userId, string password );
+      void ChangePassword( string userId, string password );
+   }
+}
