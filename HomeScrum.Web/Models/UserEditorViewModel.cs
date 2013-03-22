6923ce7996490aedff5cbb41194a147400aedd48
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-22 08:18:53 -0500

    Allow entry of password when creating new user

--- HomeScrum.Web/Models/UserEditorViewModel.cs
+++ HomeScrum.Web/Models/UserEditorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using HomeScrum.Data.Domain;
@@ -11,7 +12,6 @@ namespace HomeScrum.Web.Models
       public UserEditorViewModel( User user )
       {
          User = user;
-         Password = new LocalPasswordModel();
       }
 
       public UserEditorViewModel()
@@ -19,6 +19,16 @@ namespace HomeScrum.Web.Models
 
 
       public User User { get; set; }
-      public LocalPasswordModel Password { get; set; }
+
+      [Required]
+      [StringLength( 100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6 )]
+      [DataType( DataType.Password )]
+      [Display( Name = "Password" )]
+      public string Password { get; set; }
+
+      [DataType( DataType.Password )]
+      [Display( Name = "Confirm password" )]
+      [Compare( "Password", ErrorMessage = "The new password and confirmation password do not match." )]
+      public string ConfirmPassword { get; set; }
    }
 }
