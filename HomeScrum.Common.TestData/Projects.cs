5e5db149f8fd9a464415bc8649958749aabe9b3c
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-24 10:35:22 -0500

    Test out the project repository.

---
+++ HomeScrum.Common.TestData/Projects.cs
@@ -0,0 +1,60 @@
+﻿using HomeScrum.Data.Domain;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeScrum.Common.TestData
+{
+   public class Projects
+   {
+      public static void Load()
+      {
+         using (ISession session = Database.GetSession())
+         using (ITransaction transaction = session.BeginTransaction())
+         {
+            foreach (var project in ModelData)
+               session.Save( project );
+            transaction.Commit();
+         }
+      }
+
+      public static readonly Project[] ModelData = new[]
+      {
+         new Project ()
+         {
+            Id=Guid.NewGuid(),
+            Name="Home Scrum",
+            Description = "This project right here",
+            ProjectStatusRid=ProjectStatuses.ModelData[0].Id,
+            LastModifiedUserId ="admin"
+         },
+         new Project ()
+         {
+            Id=Guid.NewGuid(),
+            Name="PRepS",
+            Description = "An old problem reporting system",
+            ProjectStatusRid=ProjectStatuses.ModelData[2].Id,
+            LastModifiedUserId ="kws"
+         },
+         new Project ()
+         {
+            Id=Guid.NewGuid(),
+            Name="MathWar",
+            Description = "A flash card math learning game",
+            ProjectStatusRid=ProjectStatuses.ModelData[1].Id,
+            LastModifiedUserId ="ams"
+         },
+         new Project ()
+         {
+            Id=Guid.NewGuid(),
+            Name="Sandwiches",
+            Description = "Make them!",
+            ProjectStatusRid=ProjectStatuses.ModelData[0].Id,
+            LastModifiedUserId ="lls"
+         }
+      };
+   }
+}
---
+++ HomeScrum.Data.UnitTest/Repositories/ProjectRepository.cs
@@ -0,0 +1,128 @@
+﻿using HomeScrum.Common.TestData;
+using HomeScrum.Data.Domain;
+using HomeScrum.Data.Repositories;
+using HomeScrum.Data.SqlServer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeScrum.Data.UnitTest.Repositories
+{
+   [TestClass]
+   public class ProjectRepository
+   {
+      [ClassInitialize]
+      public static void InitializeClass( TestContext context )
+      {
+         Database.Initialize();
+      }
+
+
+      [TestInitialize]
+      public void InitializeTest()
+      {
+         Database.Build();
+         ProjectStatuses.Load();
+         Projects.Load();
+         _repository = new Repository<Project, Guid>();
+      }
+
+      private IRepository<Project, Guid> _repository;
+
+      [TestMethod]
+      public void GetAll_ReturnsAllProjects()
+      {
+         var projects = _repository.GetAll();
+
+         Assert.AreEqual( Projects.ModelData.GetLength( 0 ), projects.Count );
+         foreach (var status in Projects.ModelData)
+         {
+            AssertCollectionContainsProject( projects, status );
+         }
+      }
+
+      [TestMethod]
+      public void GetNonExistentProject_ReturnsNull()
+      {
+         var project = _repository.Get( Guid.NewGuid() );
+
+         Assert.IsNull( project );
+      }
+
+      [TestMethod]
+      public void GetUsingDefaultGuid_ReturnsNull()
+      {
+         var project = _repository.Get( Guid.NewGuid() );
+
+         Assert.IsNull( project );
+      }
+
+      [TestMethod]
+      public void Get_ReturnsProject()
+      {
+         var project = _repository.Get( Projects.ModelData[2].Id );
+
+         AssertProjectsAreEqual( Projects.ModelData[2], project );
+      }
+
+
+      [TestMethod]
+      public void Add_AddsProjectToDatabase()
+      {
+         var project = new Project()
+         {
+            Name = "New Project",
+            Description = "New one for Insert",
+            ProjectStatusRid = ProjectStatuses.ModelData[1].Id,
+            LastModifiedUserId = "Frank"
+         };
+
+         _repository.Add( project );
+         Assert.AreEqual( Projects.ModelData.GetLength( 0 ) + 1, _repository.GetAll().Count );
+         AssertCollectionContainsProject( _repository.GetAll(), project );
+      }
+
+      [TestMethod]
+      public void Update_ModifiesNameInDatabase()
+      {
+         var project = Projects.ModelData[3];
+
+         project.Name += "Modified";
+
+         _repository.Update( project );
+
+         Assert.AreEqual( Projects.ModelData.GetLength( 0 ), _repository.GetAll().Count );
+         AssertProjectsAreEqual( project, _repository.Get( project.Id ) );
+      }
+
+      [TestMethod]
+      public void Delete_RevmovesItemFromDatabase()
+      {
+         var project = Projects.ModelData[2];
+
+         _repository.Delete( project );
+
+         Assert.AreEqual( Projects.ModelData.GetLength( 0 ) - 1, _repository.GetAll().Count );
+         Assert.IsNull( _repository.GetAll().FirstOrDefault( x => x.Id == project.Id ) );
+      }
+
+      private void AssertCollectionContainsProject( ICollection<Project> projects, Project project )
+      {
+         var statusFromCollection = projects.FirstOrDefault( x => x.Id == project.Id );
+
+         Assert.IsNotNull( statusFromCollection );
+         AssertProjectsAreEqual( project, statusFromCollection );
+      }
+
+      private static void AssertProjectsAreEqual( Project expected, Project actual )
+      {
+         Assert.AreNotSame( expected, actual );
+         Assert.AreEqual( expected.Id, actual.Id );
+         Assert.AreEqual( expected.Name, actual.Name );
+         Assert.AreEqual( expected.Description, actual.Description );
+         Assert.AreEqual( expected.ProjectStatusRid, actual.ProjectStatusRid );
+         Assert.AreEqual( expected.LastModifiedUserId, actual.LastModifiedUserId );
+      }
+   }
+}
