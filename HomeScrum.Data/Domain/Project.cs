215ec235140a4a74d8074950efaed79ce15a4708
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-24 09:26:20 -0500

    Add project and project history mappings.

---
+++ HomeScrum.Data/Domain/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeScrum.Data.Domain
+{
+   public class Project:DataObjectBase
+   {
+      public virtual Guid ProjectStatusRid { get; set; }
+
+      public virtual string LastModifiedUserId { get; set; }
+   }
+}
---
+++ HomeScrum.Data/Domain/ProjectHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeScrum.Data.Domain
+{
+   public class ProjectHistory : Project
+   {
+      public virtual Guid ProjectRid { get; set; }
+
+      public virtual DateTime HistoryTimestamp { get; set; }
+
+      public virtual int SequenceNumber { get; set; }
+   }
+}
