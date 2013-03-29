8096772cbc6d612158a3341aa4adc800ee20dcaa
Ken Sodemann <ken.sodemann@gmail.com>
2013-03-28 19:56:23 -0500

    Add project editor view model

---
+++ HomeScrum.Web/Models/ProjectEditorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HomeScrum.Data.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeScrum.Web.Models
+{
+   public class ProjectEditorViewModel : DomainObjectEditorViewModel<Project>
+   {
+      public IEnumerable<SelectListItem> ProjectStatuses { get; set; }
+   }
+}
