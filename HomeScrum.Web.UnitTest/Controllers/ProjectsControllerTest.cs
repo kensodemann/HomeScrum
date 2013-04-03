38c9e4e4a43be946a6808312cb6041704ed714bb
Ken Sodemann <ken.sodemann@gmail.com>
2013-04-02 20:50:37 -0500

    Start tests.

--- HomeScrum.Web.UnitTest/Controllers/ProjectsControllerTest.cs
+++ HomeScrum.Web.UnitTest/Controllers/ProjectsControllerTest.cs
@@ -6,11 +6,92 @@ using System.Collections.Generic;
 using HomeScrum.Common.TestData;
 using HomeScrum.Web.Controllers;
 using System.Web.Mvc;
+using Moq;
+using HomeScrum.Data.Validators;
+using HomeScrum.Data.Repositories;
 
 namespace HomeScrum.Web.UnitTest.Controllers
 {
    [TestClass]
    public class ProjectsControllerTest
    {
+      private Mock<IRepository<Project, Guid>> _projectRepository;
+      private Mock<IRepository<ProjectStatus, Guid>> _projectStatusRepository;
+      private Mock<IValidator<Project>> _validator;
+      private ProjectsController _controller;
+
+      [TestInitialize]
+      public virtual void InitializeTest()
+      {
+         Users.CreateTestModelData( initializeIds: true );
+         ProjectStatuses.CreateTestModelData( initializeIds: true );
+         Projects.CreateTestModelData( initializeIds: true );
+
+         _projectRepository = new Mock<IRepository<Project, Guid>>();
+         _projectStatusRepository = new Mock<IRepository<ProjectStatus, Guid>>();
+         _validator = new Mock<IValidator<Project>>();
+
+         _validator.Setup( x => x.ModelIsValid( It.IsAny<Project>(), It.IsAny<TransactionType>() ) ).Returns( true );
+
+         _controller = new ProjectsController( _projectRepository.Object, _validator.Object );
+      }
+
+      [TestMethod]
+      public void Index_ReturnsViewWithModel()
+      {
+         _projectRepository.Setup( x => x.GetAll() )
+            .Returns( Projects.ModelData );
+
+         var view = _controller.Index() as ViewResult;
+
+         Assert.IsNotNull( view );
+         Assert.IsNotNull( view.Model );
+      }
+
+      [TestMethod]
+      public void Index_GetsAllItems()
+      {
+         _controller.Index();
+
+         _projectRepository.Verify( x => x.GetAll(), Times.Once() );
+      }
+
+      [TestMethod]
+      public void Details_ReturnsViewWithModel()
+      {
+         var model = Projects.ModelData[2];
+
+         _projectRepository.Setup( x => x.Get( model.Id ) )
+            .Returns( model );
+
+         var view = _controller.Details( model.Id ) as ViewResult;
+
+         _projectRepository.Verify( x => x.Get( model.Id ), Times.Once() );
+
+         Assert.IsNotNull( view );
+         Assert.IsNotNull( view.Model );
+         Assert.AreEqual( model, view.Model );
+      }
+
+      [TestMethod]
+      public void Details_ReturnsHttpNotFoundIfNoModel()
+      {
+         var id = Guid.NewGuid();
+
+         _projectRepository.Setup( x => x.Get( id ) ).Returns( null as Project );
+
+         var result = _controller.Details( id ) as HttpNotFoundResult;
+
+         Assert.IsNotNull( result );
+      }
+
+      [TestMethod]
+      public void CreateGet_ReturnsViewWithViewWithoutModel()
+      {
+         var result = _controller.Create() as ViewResult;
+
+         Assert.IsNotNull( result );
+         Assert.IsNull( result.Model );
+      }
    }
 }
