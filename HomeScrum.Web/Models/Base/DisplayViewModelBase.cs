c35fead4c18d97fff18b109b63f7ad6850f1707b
Ken Sodemann <ken.sodemann@gmail.com>
2013-04-05 16:20:51 -0500

    Create base view models.

---
+++ HomeScrum.Web/Models/Base/DisplayViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeScrum.Web.Models.Base
+{
+   // DisplayViewModelBase and EditorViewModelBase are identicle, but they are the
+   // roots of different tress.  This allows us to have two families of view models
+   // for AutoMapper to map to.
+   //
+   // That is, we can do the following and have the mapper give us the correct type
+   // of display view model based on the type for our model.
+   //   var model = new ModelT();
+   //   var viewModel = Mapper.Map( model, model.GetType(), typeof( DisplayViewModelBase ) ); 
+   public class DisplayViewModelBase
+   {
+      public Guid Id { get; set; }
+   }
+}
