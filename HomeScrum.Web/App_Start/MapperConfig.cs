cebd62ef80d2d0c36357520fcfc959205b0032fe
Ken Sodemann <ken.sodemann@gmail.com>
2013-04-06 09:09:03 -0500

    Refactor the mapping

--- HomeScrum.Web/App_Start/MapperConfig.cs
+++ HomeScrum.Web/App_Start/MapperConfig.cs
@@ -13,34 +13,16 @@ namespace HomeScrum.Web
    {
       public static void RegisterMappings()
       {
-         //
-         // Domain to View Model mapping
-         Mapper.CreateMap<DomainObjectBase, DomainObjectViewModel>()
-            .Include<SystemDomainObject, SystemDomainObjectViewModel>();
-         Mapper.CreateMap<SystemDomainObject, SystemDomainObjectViewModel>()
-            .Include<AcceptanceCriteriaStatus, AcceptanceCriteriaStatusViewModel>()
-            .ForMember( dest => dest.AllowUse, opt => opt.ResolveUsing<AllowUseResolver>() );
+         MapDomainObjectsToViewModels();
+         MapDomainObjectsToEditorViewModels();
 
-         Mapper.CreateMap<AcceptanceCriteriaStatus, AcceptanceCriteriaStatusViewModel>();
+         MapViewModelsToDomainObjects();
+         MapEditorViewModelsToDomainObjects();
+      }
 
-         Mapper.CreateMap<DomainObjectBase, DomainObjectEditorViewModel>()
-            .Include<SystemDomainObject, SystemDomainObjectEditorViewModel>();
-         Mapper.CreateMap<SystemDomainObject, SystemDomainObjectEditorViewModel>()
-            .Include<AcceptanceCriteriaStatus, AcceptanceCriteriaStatusEditorViewModel>()
-            .ForMember( dest => dest.AllowUse, opt => opt.ResolveUsing<AllowUseResolver>() );
 
-         Mapper.CreateMap<AcceptanceCriteriaStatus, AcceptanceCriteriaStatusEditorViewModel>();
-
-         //
-         // View Model to Domain mapping
-         Mapper.CreateMap<DomainObjectViewModel, DomainObjectBase>()
-            .Include<SystemDomainObjectViewModel, SystemDomainObject>();
-         Mapper.CreateMap<SystemDomainObjectViewModel, SystemDomainObject>()
-            .Include<AcceptanceCriteriaStatusViewModel, AcceptanceCriteriaStatus>()
-            .ForMember( dest => dest.StatusCd, opt => opt.ResolveUsing<StatusCodeResolver>() );
-
-         Mapper.CreateMap<AcceptanceCriteriaStatusViewModel, AcceptanceCriteriaStatus>();
-
+      private static void MapEditorViewModelsToDomainObjects()
+      {
          Mapper.CreateMap<DomainObjectEditorViewModel, DomainObjectBase>()
             .Include<SystemDomainObjectEditorViewModel, SystemDomainObject>();
          Mapper.CreateMap<SystemDomainObjectEditorViewModel, SystemDomainObject>()
@@ -48,6 +30,39 @@ namespace HomeScrum.Web
             .ForMember( dest => dest.StatusCd, opt => opt.ResolveUsing<EditorStatusCodeResolver>() );
 
          Mapper.CreateMap<AcceptanceCriteriaStatusEditorViewModel, AcceptanceCriteriaStatus>();
+      }
+
+      private static void MapViewModelsToDomainObjects()
+      {
+         Mapper.CreateMap<DomainObjectViewModel, DomainObjectBase>()
+            .Include<SystemDomainObjectViewModel, SystemDomainObject>();
+         Mapper.CreateMap<SystemDomainObjectViewModel, SystemDomainObject>()
+            .Include<AcceptanceCriteriaStatusViewModel, AcceptanceCriteriaStatus>()
+            .ForMember( dest => dest.StatusCd, opt => opt.ResolveUsing<StatusCodeResolver>() );
+
+         Mapper.CreateMap<AcceptanceCriteriaStatusViewModel, AcceptanceCriteriaStatus>();
+      }
+
+      private static void MapDomainObjectsToEditorViewModels()
+      {
+         Mapper.CreateMap<DomainObjectBase, DomainObjectEditorViewModel>()
+            .Include<SystemDomainObject, SystemDomainObjectEditorViewModel>();
+         Mapper.CreateMap<SystemDomainObject, SystemDomainObjectEditorViewModel>()
+            .Include<AcceptanceCriteriaStatus, AcceptanceCriteriaStatusEditorViewModel>()
+            .ForMember( dest => dest.AllowUse, opt => opt.ResolveUsing<AllowUseResolver>() );
+
+         Mapper.CreateMap<AcceptanceCriteriaStatus, AcceptanceCriteriaStatusEditorViewModel>();
+      }
+
+      private static void MapDomainObjectsToViewModels()
+      {
+         Mapper.CreateMap<DomainObjectBase, DomainObjectViewModel>()
+            .Include<SystemDomainObject, SystemDomainObjectViewModel>();
+         Mapper.CreateMap<SystemDomainObject, SystemDomainObjectViewModel>()
+            .Include<AcceptanceCriteriaStatus, AcceptanceCriteriaStatusViewModel>()
+            .ForMember( dest => dest.AllowUse, opt => opt.ResolveUsing<AllowUseResolver>() );
+
+         Mapper.CreateMap<AcceptanceCriteriaStatus, AcceptanceCriteriaStatusViewModel>();
       }
 
 
