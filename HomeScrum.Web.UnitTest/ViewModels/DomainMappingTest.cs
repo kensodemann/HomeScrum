49b78f55941da0eb0a17b64448a45283e4556184
Ken Sodemann <ken.sodemann@gmail.com>
2013-04-06 08:49:51 -0500

    Add domain to view model mapping.
    Starting with acceptance criteria status.  May or may not add tests for
    the other ones.

--- HomeScrum.Web.UnitTest/ViewModels/DomainMappingTest.cs
+++ HomeScrum.Web.UnitTest/ViewModels/DomainMappingTest.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Linq;
 using AutoMapper;
+using HomeScrum.Common.TestData;
+using HomeScrum.Data.Domain;
+using HomeScrum.Web.Models;
+using HomeScrum.Web.Models.Base;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HomeScrum.Web.UnitTest.ViewModels
@@ -11,12 +16,42 @@ namespace HomeScrum.Web.UnitTest.ViewModels
       public static void InitializeClass( TestContext context )
       {
          MapperConfig.RegisterMappings();
+         AcceptanceCriteriaStatuses.CreateTestModelData( initializeIds: true );
       }
+
 
       [TestMethod]
       public void MapperConfigurationIsValid()
       {
          Mapper.AssertConfigurationIsValid();
+      }
+
+      [TestMethod]
+      public void CanMapAcceptanceCriteriaStatus_DomainToViewModel()
+      {
+         var domainModel = AcceptanceCriteriaStatuses.ModelData.ToArray().First( x => x.StatusCd == 'A' );
+         var viewModel = Mapper.Map( domainModel, domainModel.GetType(), typeof( DomainObjectViewModel ) );
+
+         Assert.IsInstanceOfType( viewModel, typeof( AcceptanceCriteriaStatusViewModel ) );
+         Assert.IsTrue( ((AcceptanceCriteriaStatusViewModel)viewModel).AllowUse );
+      }
+
+      [TestMethod]
+      public void CanMapAcceptanceCriteriaStatus_ViewModelToDomain()
+      {
+         var viewModel = new AcceptanceCriteriaStatusViewModel()
+         {
+            Id = Guid.NewGuid(),
+            Name = "Test Me",
+            Description = "This is a test",
+            IsAccepted = true,
+            IsPredefined = false,
+            AllowUse = false
+         };
+         var domainModel = Mapper.Map( viewModel, viewModel.GetType(), typeof( DomainObjectBase ) );
+
+         Assert.IsInstanceOfType( domainModel, typeof(AcceptanceCriteriaStatus) );
+         Assert.AreEqual( 'I', ((AcceptanceCriteriaStatus)domainModel).StatusCd );
       }
    }
 }
--- HomeScrum.Web/App_Start/MapperConfig.cs
+++ HomeScrum.Web/App_Start/MapperConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using AutoMapper;
 using HomeScrum.Data.Domain;
+using HomeScrum.Web.Models;
 using HomeScrum.Web.Models.Base;
 
 namespace HomeScrum.Web
@@ -17,14 +18,20 @@ namespace HomeScrum.Web
          Mapper.CreateMap<DomainObjectBase, DomainObjectViewModel>()
             .Include<SystemDomainObject, SystemDomainObjectViewModel>();
          Mapper.CreateMap<SystemDomainObject, SystemDomainObjectViewModel>()
+            .Include<AcceptanceCriteriaStatus, AcceptanceCriteriaStatusViewModel>()
             .ForMember( dest => dest.AllowUse, opt => opt.ResolveUsing<AllowUseResolver>() );
+
+         Mapper.CreateMap<AcceptanceCriteriaStatus, AcceptanceCriteriaStatusViewModel>();
 
          //
          // View Model to Domain mapping
          Mapper.CreateMap<DomainObjectViewModel, DomainObjectBase>()
-            .Include<SystemDomainObjectViewModel, SystemDomainObject>(); ;
+            .Include<SystemDomainObjectViewModel, SystemDomainObject>();
          Mapper.CreateMap<SystemDomainObjectViewModel, SystemDomainObject>()
+            .Include<AcceptanceCriteriaStatusViewModel, AcceptanceCriteriaStatus>()
             .ForMember( dest => dest.StatusCd, opt => opt.ResolveUsing<StatusCodeResolver>() );
+
+         Mapper.CreateMap<AcceptanceCriteriaStatusViewModel, AcceptanceCriteriaStatus>();
       }
 
 
