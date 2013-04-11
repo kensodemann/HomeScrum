349194bf7d38062762a8f7d6aee55028304bd637
Ken Sodemann <ken.sodemann@gmail.com>
2013-04-11 06:38:40 -0500

    Label the dropdown list.

--- HomeScrum.Web/Controllers/AccountController.cs
+++ HomeScrum.Web/Controllers/AccountController.cs
@@ -12,7 +12,6 @@ using HomeScrum.Web.Providers;
 namespace HomeScrum.Web.Controllers
 {
    [Authorize]
-   //[InitializeSimpleMembership]
    public class AccountController : Controller
    {
       private readonly IWebSecurity WebSecurity;
