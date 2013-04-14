38be54bd5f66c64a1d8f20afbcf3c6985908713c
Ken Sodemann <ken.sodemann@gmail.com>
2013-04-14 14:53:52 -0500

    start adding work items.

---
+++ HomeScrum.Data/Domain/AcceptanceCriteria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeScrum.Data.Domain
+{
+   class AcceptanceCriteria : DomainObjectBase
+   {
+      [Required]
+      public virtual AcceptanceCriteriaStatus Status { get; set; }
+   }
+}
