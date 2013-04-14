a4a1ec0a730ce011c00b7d8873ce8b0321ef7388
Ken Sodemann <ken.sodemann@gmail.com>
2013-04-14 16:09:39 -0500

    add mappings for most of work item

---
+++ HomeScrum.Data/Domain/WorkItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeScrum.Data.Domain
+{
+   class WorkItem:DomainObjectBase
+   {
+      [Required]
+      public virtual WorkItemStatus Status { get; set; }
+
+      [Required]
+      public virtual WorkItemType WorkItemType { get; set; }
+
+      [Required]
+      public virtual Project Project { get; set; }
+
+      public virtual WorkItem ParentWorkItem { get; set; }
+
+      //public virtual Sprint Sprint { get; set; }
+
+      [Required]
+      public virtual Guid LastModifiedUserRid { get; set; }
+
+      [Required]
+      public virtual Guid CreatedByUserRid { get; set; }
+
+      public virtual Guid AssignedToUserRid { get; set; }
+   }
+}
