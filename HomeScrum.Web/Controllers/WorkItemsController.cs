d39c935c6545afceb0bdf83850de0c20df561459
Ken Sodemann <ken.sodemann@gmail.com>
2013-04-30 20:44:54 -0500

    Refactor to use base create() get method

--- HomeScrum.Web/Controllers/WorkItemsController.cs
+++ HomeScrum.Web/Controllers/WorkItemsController.cs
@@ -14,7 +14,7 @@ namespace HomeScrum.Web.Controllers
    {
       [Inject]
       public WorkItemsController( IRepository<WorkItem> repository, IRepository<WorkItemStatus> statusRepository, IRepository<WorkItemType> workItemTypeRepository,
-         IRepository<Project>projectRepository, IUserRepository userRepository, IValidator<WorkItem> validator )
+         IRepository<Project> projectRepository, IUserRepository userRepository, IValidator<WorkItem> validator )
          : base( repository, validator )
       {
          _statusRepository = statusRepository;
@@ -28,18 +28,13 @@ namespace HomeScrum.Web.Controllers
       private IRepository<Project> _projectRepository;
       private IUserRepository _userRepository;
 
-      //
-      // Get: /WorkItems/Create
-      public override ActionResult Create()
+      protected override void PopulateSelectLists( WorkItemEditorViewModel viewModel )
       {
-         var viewModel = new WorkItemEditorViewModel();
-
          viewModel.Statuses = _statusRepository.GetAll().ToSelectList();
          viewModel.WorkItemTypes = _workItemTypeRepository.GetAll().ToSelectList();
          viewModel.Projects = _projectRepository.GetAll().ToSelectList();
          viewModel.Users = _userRepository.GetAll().ToSelectList( allowUnassigned: true );
-
-         return View( viewModel );
+         base.PopulateSelectLists( viewModel );
       }
    }
 }
