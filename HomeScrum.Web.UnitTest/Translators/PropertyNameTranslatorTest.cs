946184a6ca264e2a39d94a97189c032916d1fb86
Ken Sodemann <ken.sodemann@gmail.com>
2013-05-11 07:23:06 -0500

    Refactor the property name translator

---
+++ HomeScrum.Web.UnitTest/Translators/PropertyNameTranslatorTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HomeScrum.Web.Translators;
+using HomeScrum.Data.Domain;
+
+namespace HomeScrum.Web.UnitTest.Translators
+{
+   [TestClass]
+   public class PropertyNameTranslatorTest
+   {
+      class TestClass
+      {
+         public Guid Id { get; set; }
+         public string Name { get; set; }
+         public string Description { get; set; }
+         public int GrossMargin { get; set; }
+      }
+
+      private PropertyNameTranslator _translator;
+      private TestClass _testObject;
+
+      [TestInitialize]
+      public void InitializeTest()
+      {
+         BuildTestObjects();
+         SetupTranslator();
+      }
+
+      private void BuildTestObjects()
+      {
+         _translator = new PropertyNameTranslator();
+         _testObject = new TestClass();
+      }
+
+      private void SetupTranslator()
+      {
+         _translator.AddTranslation( "GrossMargin", "Profit" );
+         _translator.AddTranslation( () => _testObject.Description, "DescriptiveText" );
+      }
+
+      [TestMethod]
+      public void PropertiesWithoutEntryTranslatesToSelf()
+      {
+         Assert.AreEqual( "Id", _translator.TraslatedName( "Id" ) );
+         Assert.AreEqual( "Id", _translator.TranslatedName( () => _testObject.Id ) );
+
+         Assert.AreEqual( "Name", _translator.TraslatedName( "Name" ) );
+         Assert.AreEqual( "Name", _translator.TranslatedName( () => _testObject.Name ) );
+      }
+
+      [TestMethod]
+      public void PropertiesWithEntriesTranslatesToEntries()
+      {
+         Assert.AreEqual( "Profit", _translator.TraslatedName( "GrossMargin" ) );
+         Assert.AreEqual( "Profit", _translator.TranslatedName( () => _testObject.GrossMargin ) );
+
+         Assert.AreEqual( "DescriptiveText", _translator.TraslatedName( "Description" ) );
+         Assert.AreEqual( "DescriptiveText", _translator.TranslatedName( () => _testObject.Description ) );
+      }
+   }
+}
---
+++ HomeScrum.Web/Translators/PropertyNameTranslator.cs
@@ -0,0 +1,45 @@
+﻿using HomeScrum.Common.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace HomeScrum.Web.Translators
+{
+   public class PropertyNameTranslator
+   {
+      public PropertyNameTranslator()
+      {
+         _propertyMap = new Dictionary<string, string>();
+      }
+
+      private Dictionary<String, String> _propertyMap;
+
+      public void AddTranslation<T>( Expression<Func<T>> fromProperty, string toPropertyName )
+      {
+         AddTranslation( ClassHelper.ExtractPropertyName( fromProperty ), toPropertyName );
+      }
+
+      public void AddTranslation( string fromPropertyName, string toPropertyName )
+      {
+         _propertyMap.Add( fromPropertyName, toPropertyName );
+      }
+
+
+      public string TranslatedName<T>( Expression<Func<T>> propertyExpression )
+      {
+         return TraslatedName( ClassHelper.ExtractPropertyName( propertyExpression ) );
+      }
+
+      public string TraslatedName( string propertyName )
+      {
+         if (_propertyMap.ContainsKey( propertyName ))
+         {
+            return _propertyMap[propertyName];
+         }
+
+         return propertyName;
+      }
+   }
+}
