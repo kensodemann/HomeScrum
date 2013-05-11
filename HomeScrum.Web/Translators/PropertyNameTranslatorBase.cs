13dcb61c6e1eb6ba7eb0d62565c140596153bc0f
Ken Sodemann <ken.sodemann@gmail.com>
2013-05-10 21:41:08 -0500

    start adding the translator class

---
+++ HomeScrum.Web/Translators/PropertyNameTranslatorBase.cs
@@ -0,0 +1,34 @@
+﻿using HomeScrum.Common.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace HomeScrum.Web.Translators
+{
+   public class PropertyNameTranslatorBase
+   {
+      public PropertyNameTranslatorBase()
+      {
+         _propertyMap = new Dictionary<string, string>();
+      }
+
+      private Dictionary<String, String> _propertyMap;
+
+      public string ViewModelPropertyName( string modelPropertyName )
+      {
+         if (_propertyMap.ContainsKey( modelPropertyName ))
+         {
+            return _propertyMap[modelPropertyName];
+         }
+
+         return modelPropertyName;
+      }
+
+      public string ViewModelPropertyName<T>( Expression<Func<T>> expression )
+      {
+         return ViewModelPropertyName( ClassHelper.ExtractPropertyName( expression ) );
+      }
+   }
+}
