b29359d744ff6de97fc8ecc75b031584e15637b7
Ken Sodemann <ken.sodemann@gmail.com>
2013-06-22 09:29:00 -0500

    Test the assignments allowed case.

--- HomeScrum.Web.UnitTest/Controllers/WorkItemsControllerTest.cs
+++ HomeScrum.Web.UnitTest/Controllers/WorkItemsControllerTest.cs
@@ -419,6 +419,17 @@ namespace HomeScrum.Web.UnitTest.Controllers
 
          _workItemRepository.Verify( x => x.Add( It.Is<WorkItem>( w => w.AssignedToUser == null ) ), Times.Once() );
       }
+
+      [TestMethod]
+      public void CreatePost_DoesNotSetAssignedToUserIdToDefault_IfAssignmentsIsAllowedForType()
+      {
+         var viewModel = CreateWorkItemEditorViewModel();
+         viewModel.WorkItemTypeId = WorkItemTypes.ModelData.First( x => x.IsTask && x.StatusCd == 'A' ).Id;
+
+         _controller.Create( viewModel, _principal.Object );
+
+         _workItemRepository.Verify( x => x.Add( It.Is<WorkItem>( w => w.AssignedToUser.Id == viewModel.AssignedToUserId ) ), Times.Once() );
+      }
       #endregion
 
 
