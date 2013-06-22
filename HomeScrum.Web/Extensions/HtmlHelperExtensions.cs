fb422b8194fc1fbd5949431af8173a9215196527
Ken Sodemann <ken.sodemann@gmail.com>
2013-06-21 22:10:16 -0500

    Add attributes extension.

--- HomeScrum.Web/Extensions/HtmlHelperExtensions.cs
+++ HomeScrum.Web/Extensions/HtmlHelperExtensions.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Xml.Linq;
 
 namespace HomeScrum.Web.Extensions
 {
@@ -19,6 +21,31 @@ namespace HomeScrum.Web.Extensions
          var result = htmlHelper.Encode( text );
          result = result.Replace( System.Environment.NewLine, "<br/>" );
          return new MvcHtmlString( result );
+      }
+
+      // Adapted from code found here: http://stackoverflow.com/questions/11285303/store-extra-value-per-item-in-drop-down-list
+      public static MvcHtmlString DropDownListWithDataAttributesFor<TModel, TValue>( this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, IEnumerable<SelectListItemWithAttributes> selectList )
+      {
+         string name = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName( ExpressionHelper.GetExpressionText( expression ) );
+
+         var selectDoc = XDocument.Parse( htmlHelper.DropDownList( name, (IEnumerable<SelectListItem>)selectList ).ToString() );
+
+         var options = from XElement el in selectDoc.Element( "select" ).Descendants()
+                       select el;
+
+         for (int i = 0; i < options.Count(); i++)
+         {
+            var option = options.ElementAt( i );
+            var attributes = selectList.ElementAt( i );
+
+            foreach (var attribute in attributes.DataAttributes)
+            {
+               option.SetAttributeValue( "data-" + attribute.Key, attribute.Value );
+            }
+         }
+
+         selectDoc.Root.ReplaceNodes( options.ToArray() );
+         return MvcHtmlString.Create( selectDoc.ToString() );
       }
    }
 }
---
+++ HomeScrum.Web/Extensions/SelectListItemWithAttributes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeScrum.Web.Extensions
+{
+   /// <summary>
+   /// Add data-* atttributes to items in a select list.
+   /// Used in conjuction with an HtmlHelper extension to put the DataAttributes in data-* attributes
+   /// 
+   /// Adapted from code found here: http://stackoverflow.com/questions/11285303/store-extra-value-per-item-in-drop-down-list
+   /// </summary>
+   public class SelectListItemWithAttributes : SelectListItem
+   {
+      public IDictionary<string, string> DataAttributes { get; set; }
+   }
+}
