0c35819df42cec6380ed51f41c2c8faf72a5c010
Ken Sodemann <ken.sodemann@gmail.com>
2013-07-05 14:27:29 -0500

    Add shell of more backlog, fix task list creation.

--- HomeScrum.Common.TestData/WorkItems.cs
+++ HomeScrum.Common.TestData/WorkItems.cs
@@ -84,16 +84,37 @@ namespace HomeScrum.Common.TestData
          //    ** Cancelled
          //
          var workItem = CreateWorkItem( "Add Unit Tests", "We have been bad programmers and have not been using TDD.  Get what we have tested.", pbi, planning, homeScrum );
-         StartNewTaskList();
+         OpenTaskList();
          var childWorkItem = CreateChildTask( workItem, "Examine Content", "Create catelog of our currently untested code.", sbi, newWorkItem );
-         StartCriteriaList();
+         OpenCriteriaList();
          AddAcceptanceCriteria( childWorkItem, "Coverage", "All testable code is covered", unverified );
          CloseCriteriaList( childWorkItem );
          childWorkItem = CreateChildTask( workItem, "Write Tests", "Write tests to cover the code.", sbi, newWorkItem );
-         StartCriteriaList();
+         OpenCriteriaList();
          AddAcceptanceCriteria( childWorkItem, "Coverage", "All testable code is covered", unverified );
          AddAcceptanceCriteria( childWorkItem, "Passing", "All tests pass", unverified );
          CloseCriteriaList( childWorkItem );
+         CloseTaskList( workItem );
+
+         workItem = CreateWorkItem( "Sprint Retrospective", "As a manager, I want to gather information on what went well with a sprint and what did not in order to improve the process",
+            pbi, assigned, homeScrum );
+         OpenTaskList();
+         CloseTaskList( workItem );
+
+         workItem = CreateWorkItem( "Problem Report Printing", "As a user, I want to be able to print hard copies of problem reports", pbi, complete, preps );
+         OpenTaskList();
+         CloseTaskList( workItem );
+
+         workItem = CreateWorkItem( "Hummus Sandwich", "As a vegan, I want a tasty, tasty sandwich without any animal product in it", pbi, assigned, sandwiches );
+         OpenTaskList();
+         CloseTaskList( workItem );
+
+         workItem = CreateWorkItem( "Burndown Chart", "As a user, I want a quick and easy indication of the progress of work on a sprint", customerRequest, assigned, homeScrum );
+         OpenTaskList();
+         CloseTaskList( workItem );
+
+         workItem = CreateWorkItem( "Quadratic Equations", "As a parent, I want to teach my child to sovle quadradic equations", customerRequest, cancelled, mathWar );
+         OpenTaskList();
          CloseTaskList( workItem );
 
          // 2 PBI's and 3 CR's without tasks
@@ -117,14 +138,14 @@ namespace HomeScrum.Common.TestData
          // * SBIs (5)
          // Various statuses, all SBI's either new or planning, two of each with Acceptance Criteria (Untested)
          workItem = CreateWorkItem( "No LDAP Server", "I cannot actually work on the LDAP server related stuff until I have a test server", issue, assigned, preps );
-         StartCriteriaList();
+         OpenCriteriaList();
          AddAcceptanceCriteria( workItem, "Exists", "The LDAP Server exists", accepted );
          AddAcceptanceCriteria( workItem, "Accessible", "I can access the LDAP Server without error", rejected );
          AddAcceptanceCriteria( workItem, "Contains Data", "The LDAP server has test data I can use", unverified );
          CloseCriteriaList( workItem );
          workItem = CreateWorkItem( "Modly Bread", "I cannot make a BLT if all of the bread has mold on it.", issue, assigned, sandwiches );
          workItem = CreateWorkItem( "Remainders", "No one has defined how remainders should be dealt with.", issue, cancelled, mathWar );
-         StartCriteriaList();
+         OpenCriteriaList();
          AddAcceptanceCriteria( workItem, "Playable", "The way remainders are to be handled is always playable by the user", unverified );
          AddAcceptanceCriteria( workItem, "Configurable", "The specification allows for user configuration", unverified );
          CloseCriteriaList( workItem );
@@ -132,7 +153,7 @@ namespace HomeScrum.Common.TestData
 
          workItem = CreateWorkItem( "No Admin", "It is possible to set all users to not be admins, which makes it impossible to then administer the system", bug, complete, preps );
          workItem = CreateWorkItem( "Bug in Soup", "Waiter, there is a bug in my soup.", bug, newWorkItem, sandwiches );
-         StartCriteriaList();
+         OpenCriteriaList();
          AddAcceptanceCriteria( workItem, "Removed", "The current bowl of soup is removed", accepted );
          AddAcceptanceCriteria( workItem, "New Soup", "A new bowl of soup is delivered to the table, not just the old soup with the bug removed", unverified );
          AddAcceptanceCriteria( workItem, "No Bug", "There is no bug in the new bowl of soup", unverified );
@@ -141,7 +162,7 @@ namespace HomeScrum.Common.TestData
          workItem = CreateWorkItem( "Addition Error", "The program seems to randomly think that 2 + 2 = 8.", bug, assigned, mathWar );
          workItem = CreateWorkItem( "Cannot assign sprint", "Attempt to assign an SBI to a sprint, the sprint does not stay selected.  It is not being saved in the database.", bug, assigned, homeScrum );
          workItem = CreateWorkItem( "Hide not working", "The hiding of completed work items is having no effect.", bug, cancelled, homeScrum );
-         StartCriteriaList();
+         OpenCriteriaList();
          AddAcceptanceCriteria( workItem, "Hide", "Completed work items are hidden when hide is active", accepted );
          AddAcceptanceCriteria( workItem, "Show", "Completed work items are shown when hide is not active", accepted );
          AddAcceptanceCriteria( workItem, "No Server Hit", "The server is not hit at all during a show or a hide", accepted );
@@ -151,13 +172,13 @@ namespace HomeScrum.Common.TestData
          workItem = CreateWorkItem( "Add Last Active Admin Edit", "When a user is made not an admin, make sure they are not the last active admin user.", sbi, newWorkItem, preps );
          workItem = CreateWorkItem( "Sprint Model", "Implement the domain model for sprints", sbi, newWorkItem, homeScrum );
          workItem = CreateWorkItem( "Add backlog to sprint", "Modify the sprint screen to allow the addition of one or more backlog items", sbi, planning, homeScrum );
-         StartCriteriaList();
+         OpenCriteriaList();
          AddAcceptanceCriteria( workItem, "All Tasks Added", "All tasks associated with the backlog item are automatically added to the sprint", unverified );
          AddAcceptanceCriteria( workItem, "Estimate Updated", "The estimate for the completion of the sprint is updated based on the estimates on the individual tasks", unverified );
          CloseCriteriaList( workItem );
          workItem = CreateWorkItem( "Add Mayo", "Spread mayo on the toast", sbi, planning, sandwiches );
          workItem = CreateWorkItem( "Add Division Configuration", "Update the configuration screen to include a configuration section.  Save the configuration to the INI", sbi, planning, mathWar );
-         StartCriteriaList();
+         OpenCriteriaList();
          AddAcceptanceCriteria( workItem, "Remainder Setting", "Are remainders entered? toggle.", unverified );
          AddAcceptanceCriteria( workItem, "Difficulty Setting", "Difficulting setting (easy, medium, difficult)", unverified );
          AddAcceptanceCriteria( workItem, "Defaults", "Remainder defaults to Yes.  Difficulty defaults to medium", unverified );
@@ -286,7 +307,7 @@ namespace HomeScrum.Common.TestData
          return workItem;
       }
 
-      private static void StartNewTaskList()
+      private static void OpenTaskList()
       {
          _tasks = new List<WorkItem>();
       }
@@ -312,16 +333,17 @@ namespace HomeScrum.Common.TestData
          };
 
          _workItems.Add( workItem );
+         _tasks.Add( workItem );
 
          return workItem;
       }
 
-      private static void StartCriteriaList()
+      private static void OpenCriteriaList()
       {
          _criteria = new List<AcceptanceCriteria>();
       }
 
-      private static void CloseCriteriaList(WorkItem workItem)
+      private static void CloseCriteriaList( WorkItem workItem )
       {
          workItem.AcceptanceCriteria = _criteria.ToArray();
          _criteria = null;
