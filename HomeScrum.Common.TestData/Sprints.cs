4ca62a004002ac2b1f88d972bf2d10bb557ac08b
Ken Sodemann <ken.sodemann@gmail.com>
2013-08-11 07:28:09 -0500

    Add more sprints.

--- HomeScrum.Common.TestData/Sprints.cs
+++ HomeScrum.Common.TestData/Sprints.cs
@@ -32,14 +32,24 @@ namespace HomeScrum.Common.TestData
 
       private static void CreateTestModelData( ISessionFactory sessionFactory )
       {
+         var project1 = Projects.ModelData.First( x => x.Status.IsActive );
+         var project2 = Projects.ModelData.First( x => x.Status.IsActive && x.Id != project1.Id );
+         var inactiveProject = Projects.ModelData.First( x => !x.Status.IsActive );
+
+         var prePlanning = SprintStatuses.ModelData.First( x => x.Name == "Pre Planning" );
+         var planning = SprintStatuses.ModelData.First( x => x.Name == "Planning" );
+         var inProcess = SprintStatuses.ModelData.First( x => x.Name == "In Process" );
+         var retrospective = SprintStatuses.ModelData.First( x => x.Name == "Retrospective" );
+         var closed = SprintStatuses.ModelData.First( x => x.Name == "Closed" );
+
          ModelData = new[]
          {
             new Sprint()
             {
-               Name = "Project 0, Sprint 1",
-               Description = "The first sprint for the 0th project in the list",
-               Status = SprintStatuses.ModelData.First(x=>x.Name == "Closed"),
-               Project = Projects.ModelData[0],
+               Name = "Project 1, Sprint 1",
+               Description = "The first sprint for the 1st active project",
+               Status = closed,
+               Project = project1,
                StartDate = new DateTime(2013, 1, 1),
                EndDate = new DateTime(2013,1,31),
                Goal = "Get the initial design finalized",
@@ -47,10 +57,10 @@ namespace HomeScrum.Common.TestData
             },
             new Sprint()
             {
-               Name = "Project 0, Sprint 2",
-               Description = "The second sprint for the 0th project in the list",
-               Status = SprintStatuses.ModelData.First(x=>x.Name == "Closed"),
-               Project = Projects.ModelData[0],
+               Name = "Project 1, Sprint 2",
+               Description = "The second sprint for the 1st active project",
+               Status = closed,
+               Project = project1,
                StartDate = new DateTime(2013, 2, 1),
                EndDate = new DateTime(2013,2,28),
                Goal = "Create Domains Models and ORM Mappings",
@@ -58,10 +68,10 @@ namespace HomeScrum.Common.TestData
             },
             new Sprint()
             {
-               Name = "Project 0, Sprint 3",
-               Description = "The third sprint for the 0th project in the list",
-               Status = SprintStatuses.ModelData.First(x=>x.Name == "Retrospective"),
-               Project = Projects.ModelData[0],
+               Name = "Project 1, Sprint 3",
+               Description = "The third sprint for the 1st active project",
+               Status = retrospective,
+               Project = project1,
                StartDate = new DateTime(2013, 3, 1),
                EndDate = new DateTime(2013,3,31),
                Goal = "Develop pattern for the controller classes",
@@ -69,10 +79,10 @@ namespace HomeScrum.Common.TestData
             },
             new Sprint()
             {
-               Name = "Project 0, Sprint 4",
-               Description = "The forth sprint for the 0th project in the list",
-               Status = SprintStatuses.ModelData.First(x=>x.Name == "Planning"),
-               Project = Projects.ModelData[0],
+               Name = "Project 1, Sprint 4",
+               Description = "The forth sprint for the 1st active project",
+               Status = planning,
+               Project = project1,
                StartDate = new DateTime(2013, 4, 1),
                EndDate = null,
                Goal = "Expand controllers",
@@ -80,13 +90,57 @@ namespace HomeScrum.Common.TestData
             },
             new Sprint()
             {
-               Name = "Project 0, Sprint 5",
-               Description = "The fifth sprint for the 0th project in the list",
-               Status = SprintStatuses.ModelData.First(x=>x.Name == "Pre Planning"),
-               Project = Projects.ModelData[0],
+               Name = "Project 1, Sprint 5",
+               Description = "The fifth sprint for the 1st active project",
+               Status = prePlanning,
+               Project = project1,
                StartDate = null,
                EndDate = null,
                Goal = "Create editor views",
+               LastModifiedUserRid = Users.ModelData[1].Id
+            },
+            new Sprint()
+            {
+               Name = "Project 2, Sprint 1",
+               Description = "The first sprint for the 2nd active project",
+               Status = prePlanning,
+               Project = project2,
+               StartDate = null,
+               EndDate = null,
+               Goal = "Create editor views",
+               LastModifiedUserRid = Users.ModelData[1].Id
+            },
+            new Sprint()
+            {
+               Name = "Inactive Project, Sprint 1",
+               Description = "The first sprint for the inactive project",
+               Status = closed,
+               Project = inactiveProject,
+               StartDate = new DateTime(2012, 6, 15),
+               EndDate = new DateTime(2012,7,14),
+               Goal = "Accomplish something",
+               LastModifiedUserRid = Users.ModelData[1].Id
+            },
+            new Sprint()
+            {
+               Name = "Inactive Project, Sprint 2",
+               Description = "The second sprint for the inactive project",
+               Status = closed,
+               Project = inactiveProject,
+               StartDate = new DateTime(2012, 7, 15),
+               EndDate = new DateTime(2012,8,14),
+               Goal = "Accomplish something else",
+               LastModifiedUserRid = Users.ModelData[1].Id
+            },
+            new Sprint()
+            {
+               Name = "Inactive Project, Sprint 3",
+               Description = "The third sprint for the inactive project",
+               Status = inProcess,
+               Project = inactiveProject,
+               StartDate = new DateTime(2012, 7, 15),
+               EndDate = null,
+               Goal = "The project is imploding, attempt to prevent that",
                LastModifiedUserRid = Users.ModelData[1].Id
             }
          };
