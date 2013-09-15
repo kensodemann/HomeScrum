6ff6f149435a25fea3a381f1254f9529f5c844f4
Ken Sodemann <ken.sodemann@gmail.com>
2013-09-14 20:19:14 -0500

    Add descriptions.

--- HomeScrum.Data/Domain/WorkItemTypeCategory.cs
+++ HomeScrum.Data/Domain/WorkItemTypeCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace HomeScrum.Data.Domain
 {
    public enum WorkItemTypeCategory
    {
+      [Description( "Backlog Item - Product Features and Requirements" )]
       BacklogItem,
+
+      [Description( "Task - Work required to accomplish features" )]
       Task,
+
+      [Description( "Issue - A Problem or Bug" )]
       Issue
    }
 }
