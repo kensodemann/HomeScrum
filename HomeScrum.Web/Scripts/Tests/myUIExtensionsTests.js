0c3138c05612fd15f48a81e3c908855e5703c970
Ken Sodemann <ken.sodemann@gmail.com>
2013-10-21 21:01:46 -0500

    Add local storage tests.

--- HomeScrum.Web/Scripts/Tests/myUIExtensionsTests.js
+++ HomeScrum.Web/Scripts/Tests/myUIExtensionsTests.js
@@ -6,6 +6,7 @@
    }
 });
 
+// Button Label Tests
 test('Button Initially Says Show', function () {
    setupShowHideButton("Test", $("#ShowHideToggle"), "Foo");
    strictEqual($("#ShowHideToggleLabel").text(), "Show Foo", "Button Label: Show Foo");
@@ -24,6 +25,7 @@ test('Button Toggles Back to Show After Another Click', function () {
    strictEqual($("#ShowHideToggleLabel").text(), "Show Foo", "Button Label: Show Foo");
 });
 
+// Hide/Show elements test
 test('Initial State Hides Elements', function () {
    setupShowHideButton("Test", $("#ShowHideToggle"), "Foo");
    strictEqual($(".FooItemRow:hidden").length, 2, "Foo Items are Hidden");
@@ -44,5 +46,32 @@ test('Elements Hidden After Another Click', function () {
    $(".FooItemRow").each(function () {
       strictEqual($(this).css('opacity'), '0', "Foo Items are going opaque");
    });
+   strictEqual($(".BarItemRow:hidden").length, 0, "Bar Items are not hidden");
+});
+
+// Local Storage Write
+test('Show/Hide State starts out null', function () {
+   setupShowHideButton("Test", $("#ShowHideToggle"), "Foo");
+   strictEqual($.localStorage("TestShowFoo"), null, "Show Is Stored");
+});
+
+test('Show State Stored in Local Storage', function () {
+   setupShowHideButton("Test", $("#ShowHideToggle"), "Foo");
+   $("#ShowHideToggle").click();
+   strictEqual($.localStorage("TestShowFoo"), true, "Show Is Stored");
+});
+
+test('Hide State Stored in Local Storage', function () {
+   setupShowHideButton("Test", $("#ShowHideToggle"), "Foo");
+   $("#ShowHideToggle").click();
+   $("#ShowHideToggle").click();
+   strictEqual($.localStorage("TestShowFoo"), false, "Show Is Stored");
+});
+
+// Local Storage Read
+test('Initial State Shows Elements if TestShowFoo is true', function () {
+   $.localStorage("TestShowFoo", true);
+   setupShowHideButton("Test", $("#ShowHideToggle"), "Foo");
+   strictEqual($(".FooItemRow:hidden").length, 0, "Foo Items are Hidden");
    strictEqual($(".BarItemRow:hidden").length, 0, "Bar Items are not hidden");
 });
