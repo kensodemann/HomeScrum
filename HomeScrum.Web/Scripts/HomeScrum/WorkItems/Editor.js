c693fa11039ba35d4e1887e7d229d2aa92fae5a1
Ken Sodemann <ken.sodemann@gmail.com>
2013-10-23 18:26:03 -0500

    Define basic structure that will be followed.

--- HomeScrum.Web/Scripts/HomeScrum/WorkItems/Editor.js
+++ HomeScrum.Web/Scripts/HomeScrum/WorkItems/Editor.js
@@ -1,1 +1,21 @@
-﻿
+﻿function Editor() {
+
+   var init = function () {
+   };
+
+   var test = function () {
+      return "test";
+   }
+
+   var echo = function (val) {
+      return val;
+   }
+
+   var vm = {
+      init: init,
+      test: test,
+      echo: echo
+   };
+
+   return vm;
+}
--- HomeScrum.Web/Scripts/Tests/WorkItems/EditorTests.js
+++ HomeScrum.Web/Scripts/Tests/WorkItems/EditorTests.js
@@ -1,1 +1,7 @@
-﻿
+﻿test('Test', function () {
+   strictEqual(Editor().test(), 'test');
+});
+
+test('Echo Test', function () {
+   strictEqual(Editor().echo('this is a test'), 'this is a test');
+});
