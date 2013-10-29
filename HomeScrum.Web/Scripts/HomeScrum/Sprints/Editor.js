6884b411bd67f57f3238c95f62d2b897783ca0ca
Ken Sodemann <ken.sodemann@gmail.com>
2013-10-28 21:43:58 -0500

    Create infrastructure for Sprint Editor JavaScript.

---
+++ HomeScrum.Web/Scripts/HomeScrum/Sprints/Editor.js
@@ -0,0 +1,17 @@
+﻿var Editor = (function () {
+
+   function ShowHideDataItems() {
+   }
+
+   function SetAccess() {
+   }
+
+   var init = function () {
+      ShowHideDataItems();
+      SetAccess();
+   };
+
+   return {
+      init: init
+   };
+})();
---
+++ HomeScrum.Web/Scripts/Tests/Sprints/EditorTests.js
@@ -0,0 +1,18 @@
+﻿// The following items are enable/disabled based on sprint status
+//   * Name
+//   * Description
+//   * Goal
+//   * Name
+//   * Project Id
+//
+// The following are shown/hid based on sprint status
+//   * backlog link
+//   * task link
+//
+// Hide the backlog link if backlog is closed
+// Hide the task link if task list is closed
+//
+// The Project Id is disabled if a backlog item is associated with the sprint
+// The Project Id is disabled if a task is associated with the sprint
+//
+// Sync Project Id
