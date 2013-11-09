0d6664a0592966e5738005fe977adc1df0ed44da
Ken Sodemann <ken.sodemann@gmail.com>
2013-11-09 08:28:04 -0600

    Enable/Disable parent data based on state.
    NOTE: This needs another round of tests (Create and Edit modes).

--- HomeScrum.Web/Scripts/HomeScrum/Base/EditorBase.js
+++ HomeScrum.Web/Scripts/HomeScrum/Base/EditorBase.js
@@ -7,6 +7,7 @@
 
    function init() {
       SetSubmitButtonText();
+      SetMainDataAccess();
    }
 
    function SetSubmitButtonText() {
@@ -17,6 +18,39 @@
          $("#SubmitButton").text("Done Editing");
       } else if (editMode === "Create") {
          $("#SubmitButton").text("Create");
+      }
+   }
+
+   function SetMainDataAccess() {
+      var els = $(".MainData");
+      if ($("#EditMode").val() === 'ReadOnly') {
+         for (var i = 0; i < els.length; i++) {
+            Disable(els[i]);
+         }
+      } else {
+         for (var i = 0; i < els.length; i++) {
+            Enable(els[i]);
+         }
+      }
+   }
+
+   function Disable(element) {
+      var el = $(element);
+      if (el.is(":text")) {
+         el.prop('readonly', true);
+         el.addClass('disabled');
+      } else if (el.is(':input')) {
+         el.prop('disabled', true);
+      }
+   }
+
+   function Enable(element) {
+      var el = $(element);
+      if (el.is(":text")) {
+         el.prop('readonly', false);
+         el.removeClass('disabled');
+      } else if (el.is(':input')) {
+         el.prop('disabled', false);
       }
    }
 
--- HomeScrum.Web/Scripts/Tests/Base/EditorBaseTests.js
+++ HomeScrum.Web/Scripts/Tests/Base/EditorBaseTests.js
@@ -1,4 +1,5 @@
-﻿test('Submit Button label is Edit if mode is ReadOnly on init', function () {
+﻿// Initial state tests
+test('Submit Button label is Edit if mode is ReadOnly on init', function () {
    $("#EditMode").val('ReadOnly');
    EditorBase.init();
    strictEqual($('#SubmitButton').text(), 'Edit');
@@ -15,3 +16,33 @@ test('Submit Button label is Create if mode is Create on init', function () {
    EditorBase.init();
    strictEqual($('#SubmitButton').text(), 'Create');
 });
+
+test('Main Data text inputs read-only on init if EditMode is ReadOnly', function () {
+   $('#EditMode').val('ReadOnly');
+   EditorBase.init();
+   strictEqual($("#ParentText").prop("readonly"), true, "ParentText is Readonly");
+   ok($("#ParentText").hasClass("disabled"), "ParentText has Disabled Class");
+});
+
+test('Main Data non-text inputs disbled on init if EditMode is ReadOnly', function () {
+   $('#EditMode').val('ReadOnly');
+   EditorBase.init();
+   strictEqual($("#ParentCheckbox").prop('disabled'), true, "ParentCheckbox is disabled");
+   strictEqual($("#ParentSelect").prop('disabled'), true, "ParentSelect is disabled");
+});
+
+test('Child Data text inputs not read-only on init if EditMode is ReadOnly', function () {
+   $('#EditMode').val('ReadOnly');
+   EditorBase.init();
+   strictEqual($("#ChildText").prop("readonly"), false, "ChildText not Readonly");
+   ok(!($("#ChildText").hasClass("disabled")), "ChildText does not have Disabled Class");
+});
+
+test('Main Data non-text inputs not disabled on init if EditMode is ReadOnly', function () {
+   $('#EditMode').val('ReadOnly');
+   EditorBase.init();
+   strictEqual($("#ChildCheckbox").prop('disabled'), false, "ChildCheckbox is not disabled");
+   strictEqual($("#ChildSelect").prop('disabled'), false, "ChildSelect is not disabled");
+});
+
+// "Button" press 6tests
