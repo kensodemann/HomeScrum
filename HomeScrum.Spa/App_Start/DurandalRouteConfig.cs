2d3abd656263459d46b320119053dc2699891e3d
Ken Sodemann <ken.sodemann@gmail.com>
2013-12-07 07:29:02 -0600

    Install durandal.

---
+++ HomeScrum.Spa/App_Start/DurandalRouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+[assembly: WebActivator.PreApplicationStartMethod(
+    typeof( HomeScrum.Spa.App_Start.DurandalRouteConfig ), "RegisterDurandalPreStart", Order = 2 )]
+
+namespace HomeScrum.Spa.App_Start
+{
+   public static class DurandalRouteConfig
+   {
+      public static void RegisterDurandalPreStart()
+      {
+
+         // Preempt standard default MVC page routing to go to HotTowel Sample
+         System.Web.Routing.RouteTable.Routes.MapRoute(
+             name: "DurandalMvc",
+             url: "{controller}/{action}/{id}",
+             defaults: new
+             {
+                controller = "Durandal",
+                action = "Index",
+                id = UrlParameter.Optional
+             }
+         );
+      }
+   }
+}
