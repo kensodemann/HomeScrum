4575c5ffc395f3cb591fd8c0e64ef1cce6d6e923
Ken Sodemann <ken.sodemann@gmail.com>
2013-12-09 14:49:52 -0600

    Change the title.

--- HomeScrum.Spa/App/main.js
+++ HomeScrum.Spa/App/main.js
@@ -15,7 +15,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
     system.debug(true);
     //>>excludeEnd("build");
 
-    app.title = 'Durandal Starter Kit';
+    app.title = 'Home Scrum';
 
     app.configurePlugins({
         router: true,
