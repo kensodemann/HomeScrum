bcbf74654735c7879dc273cd69a388bbcb74d420
Ken Sodemann <ken.sodemann@gmail.com>
2013-12-14 14:57:53 -0600

    Add several controllers.  Add authorization requirement.

--- HomeScrum.Spa/Controllers/DomainObjectApiController.cs
+++ HomeScrum.Spa/Controllers/DomainObjectApiController.cs
@@ -12,6 +12,7 @@ using System.Web.Http;
 
 namespace HomeScrum.Spa.Controllers
 {
+   [Authorize]
    public class DomainObjectApiController<T> : ApiController
       where T: DomainObjectBase
    {
