9a26d01f25738b0351c8608ca921ce7eda9052a2
Ken Sodemann <ken.sodemann@gmail.com>
2013-12-28 08:51:26 -0600

    Map the work item daily snapshot view.

--- HomeScrum.Data/Domain/WorkItemDailySnapshot.cs
+++ HomeScrum.Data/Domain/WorkItemDailySnapshot.cs
@@ -8,10 +8,36 @@ namespace HomeScrum.Data.Domain
 {
    public class WorkItemDailySnapshot
    {
-      public virtual WorkItem WorkItem { get; set; }
-      public virtual DateTime HistoryDate { get; set; }
+      public virtual Guid WorkItemRid { get; set; }
       public virtual int SortSequenceNumber { get; set; }
+      public virtual DateTime HistoryDate { get; set; }
       public virtual int Points { get; set; }
       public virtual int PointsRemaining { get; set; }
+      public virtual WorkItem WorkItem { get; set; }
+
+      public override bool Equals( object other )
+      {
+         if (this == other) return true;
+
+         var item = other as WorkItemDailySnapshot;
+
+         if (item == null || this.WorkItemRid != item.WorkItemRid || this.SortSequenceNumber != item.SortSequenceNumber)
+         {
+            return false;
+         }
+
+         return true;
+      }
+
+      public override int GetHashCode()
+      {
+         unchecked
+         {
+            int result;
+            result = WorkItemRid.GetHashCode();
+            result = 29 * result + SortSequenceNumber.GetHashCode();
+            return result;
+         }
+      }
    }
 }
